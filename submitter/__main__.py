f8d86793b103de0c57ebdfa4431cd33ba9a3d227
Ash Wilson <smashwilson@gmail.com>
2016-04-22 15:30:40 -0400

    Entry point.

--- submitter/__main__.py
+++ submitter/__main__.py
@@ -1,8 +1,43 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+import logging
 import sys
+from datetime import datetime
 
-print('Placeholder.')
+from .config import Config
+from .submit import submit
+
+c = Config(os.environ)
+
+if c.verbose:
+    level=logging.DEBUG
+else:
+    level=logging.INFO
+logging.basicConfig(format='%(message)s', level=level)
+logging.getLogger("requests").setLevel(logging.WARNING)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+
+if not c.is_valid():
+    logging.error('Invalid configuration. Missing the following environment variables:')
+    for var in c.missing():
+        logging.error("  " + var)
+    sys.exit(1)
+
+start = datetime.utcnow()
+result = submit(c)
+finish = datetime.utcnow()
+
+pattern = 'Submitted {asset_uploaded} / {asset_total} assets and ' \
+    '{envelope_uploaded} / {envelope_total} envelopes in {duration}.'
+summary = pattern.format(
+    asset_uploaded=result.asset_result.uploaded,
+    asset_total=len(result.asset_result.asset_set),
+    envelope_uploaded=result.envelope_result.uploaded,
+    envelope_total=len(result.envelope_result.envelope_set),
+    duration=finish - start
+)
+logging.info(summary)
 
 sys.exit(1)
